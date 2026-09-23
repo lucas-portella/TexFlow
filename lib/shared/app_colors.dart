b0418e2af4dab62d15d22728dad92bf19b842259
Lucas Portella <lucasportella@hotmail.com>
2026-09-22 21:57:50 -0300

    feat: classe AppColors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blueBackground = Color(0xFF7C92B3);
+  static const mainBlue = Color(0xFF1E40AF);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
